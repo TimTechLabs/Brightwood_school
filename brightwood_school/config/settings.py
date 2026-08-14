a0c519f51b191e28ce37d0cccd3419977e5b2278
TimTechNairobi <timothysimiyu516@gmail.com>
2026-08-14 11:05:46 +0300

    hhj

--- brightwood_school/config/settings.py
+++ brightwood_school/config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-8j-)q+u=gowgiqoj=p96k=@(jy9q!i+hyrb1s@y5guz^9%3fjo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 # config/settings.py
 
